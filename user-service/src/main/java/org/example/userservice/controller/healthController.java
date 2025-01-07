e22c73398c0b7f15df6daea2137d3fd3381ca139
bkbk123a <qjarud32@naver.com>
2025-01-07 22:28:16 +0900

    health-check 추가

---
+++ user-service/src/main/java/org/example/userservice/controller/healthController.java
@@ -0,0 +1,13 @@
+package org.example.userservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class healthController {
+
+    @GetMapping("/health-check")
+    public String status() {
+        return "Works Fine";
+    }
+}
