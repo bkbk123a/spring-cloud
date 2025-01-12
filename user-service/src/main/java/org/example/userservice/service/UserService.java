7886e77922355abfb854bbc2f7149c63489f18c6
bkbk123a <qjarud32@naver.com>
2025-01-13 00:34:59 +0900

    BCryptPasswordEncoder(Spring Security) 이용한 pwd 암호화 설정

--- user-service/src/main/java/org/example/userservice/service/UserService.java
+++ user-service/src/main/java/org/example/userservice/service/UserService.java
@@ -6,6 +6,7 @@ import org.example.userservice.jpa.UserEntity;
 import org.example.userservice.repository.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -15,6 +16,7 @@ import java.util.UUID;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     public UserDto createUser(UserDto userDto) {
         userDto.setUserId(UUID.randomUUID().toString());
@@ -22,12 +24,10 @@ public class UserService {
         ModelMapper mapper = new ModelMapper();
         mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
         UserEntity userEntity = mapper.map(userDto, UserEntity.class);
-        userEntity.setEncryptedPwd("Encrypted Password");
+        userEntity.setEncryptedPwd(passwordEncoder.encode(userDto.getPwd()));
 
         userRepository.save(userEntity);
 
-        UserDto returnUserDto = mapper.map(userEntity, UserDto.class);
-
-        return returnUserDto;
+        return mapper.map(userEntity, UserDto.class);
     }
 }
