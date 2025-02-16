fe0f85eb054cd42cdedde081c567a900c208208d
bkbk123a <qjarud32@naver.com>
2025-02-16 11:01:52 +0900

    [config-service] 프로젝트 추가, apigateway,user-service 수정

--- user-service/src/main/java/com/example/userservice/controller/healthController.java
+++ user-service/src/main/java/com/example/userservice/controller/healthController.java
@@ -1,13 +1,24 @@
 package com.example.userservice.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class healthController {
 
+    private final Environment env;
+
     @GetMapping("/health-check")
     public String status() {
-        return "Works Fine";
+        return String.format("It's Working in User Service"
+                + ", port(local.server.port)=" + env.getProperty("local.server.port")
+                + ", port(server.port)=" + env.getProperty("server.port")
+                + ", gateway ip(env)=" + env.getProperty("gateway.ip")
+                + ", message=" + env.getProperty("greeting.message")
+                + ", token expiration time=" + env.getProperty("token.expiration_time"));
     }
+
 }
